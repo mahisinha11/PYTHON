6f4d963585abfa429e18186e723d77081ac407eb
mahisinha11 <mahisinha2005@gmail.com>
2025-11-30 17:11:45 +0530

    Add files via upload

---
+++ ASSIGNMENT 2.py
@@ -0,0 +1,23 @@
+TASK 1
+
+num = int(input("Enter an integer: "))
+
+
+if num % 2 == 0:
+    print(num, "is an Even number.")
+else:
+    print(num, "is an Odd number.")
+
+
+
+TASK 2
+
+total_sum = 0
+
+
+for num in range(1, 51):
+    total_sum += num
+
+
+print("The sum of numbers from 1 to 50 is:", total_sum)
+
